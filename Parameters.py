138800e2a14ca7bd61bed9fe951632550584b180
Rashmi Bhat K <38175555+freak153@users.noreply.github.com>
2024-05-10 13:48:11 +0800

    commit

---
+++ Parameters.py
@@ -0,0 +1,11 @@
+# Image size for processing
+IMAGE_SIZE = 50
+# Training Data
+TRAINING_DATA = '/home/lenovo/Plant Disease Detection/train'
+# Testing Data
+TESTING_DATA = '/home/lenovo/Plant Disease Detection/test'
+# Model learning rate
+LEARNING_RATE = 1e-3
+# Name of the model
+DEEP_LEARNING_MODEL = 'doctor_plant-{}-{}.model'.format('cnn', LEARNING_RATE)
+
